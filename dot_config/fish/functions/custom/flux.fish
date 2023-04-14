94dcbb9bb6b4d75f79e67d19955188733631d790
Rafael Ribeiro <rafael.ntw@gmail.com>
2023-04-14 10:59:59 +0100

    Update .config/fish/functions/custom/flux.fish Update .config/fish/functions/custom/kubernetes.fish

--- dot_config/fish/functions/custom/flux.fish
+++ dot_config/fish/functions/custom/flux.fish
@@ -1,4 +1,4 @@
-# Flux Reconcile Terraform
+fl# Flux Reconcile Terraform
 # usage: frt authentik will run flux reconcile source oci terraform-authentik-oci
 function frt --description "frt authentik will run flux reconcile source oci terraform-authentik-oci"
     set argument $argv[1]
--- dot_config/fish/functions/custom/kubernetes.fish
+++ dot_config/fish/functions/custom/kubernetes.fish
@@ -1,13 +1,14 @@
 # Update Kubeconfig
 # Takes all yaml files under ~/.kube/ and adds them together in a combined config file.
 function ukf
-    set -l backup_dir ~/.kube/backup
-    set -l date (date -u +'%Y-%m-%dT%H:%M:%SZ')
+    set -x backup_dir ~/.kube/backup
+    set -x date (date -u +'%Y-%m-%dT%H:%M:%SZ')
     mkdir -p $backup_dir
     echo $date
     mv ~/.kube/config $backup_dir/config-$date
-    if test -n ~/.kube/*.yaml
-        set -x KUBECONFIG (echo ~/.kube/*.yaml | tr ' ' ':')
+    set -l yaml_files (ls ~/.kube/*.yaml)
+    if test -n "$yaml_files"
+        set -x KUBECONFIG (echo $yaml_files | tr ' ' ':')
         kubectl config view --merge --flatten > ~/.kube/config
         echo "KUBECONFIG updated"
     else
