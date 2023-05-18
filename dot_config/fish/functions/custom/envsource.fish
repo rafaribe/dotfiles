c45856be8f1ae963b215d7e2de772d5664aa2b1e
Rafael Ribeiro <rafael.ntw@gmail.com>
2023-05-18 09:54:14 +0100

    Update .config/fish/functions/custom/envsource.fish

--- dot_config/fish/functions/custom/envsource.fish
+++ dot_config/fish/functions/custom/envsource.fish
@@ -5,3 +5,24 @@ function envsource
       echo "Exported key $item[1]"
     end
   end
+
+function set_go_variables
+    set go_env_output (go env)
+    for line in $go_env_output
+        set -l key (echo $line | awk -F'=' '{print $1}' | tr -d '"')
+        set -l value (echo $line | awk -F'=' '{print $2}' | tr -d '"')
+        switch $key
+            case "GOPATH"
+                set -gx GOPATH $value
+                set -gx GOBIN $value/bin
+            # Add more cases for other variables if needed
+        end
+    end
+
+    fish_add_path $GOBIN
+    fish_add_path $GOPATH
+end
+
+if status --is-interactive
+    set_go_variables
+end
