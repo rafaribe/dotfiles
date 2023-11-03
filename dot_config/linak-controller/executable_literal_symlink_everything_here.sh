2ba0f0608aba0642616b6e6b830fbe96972e68f7
Rafael Ribeiro <rafael.ntw@gmail.com>
2023-11-03 10:58:57 +0000

    Update .config/linak-controller/symlink_everything_here.sh

--- dot_config/linak-controller/executable_literal_symlink_everything_here.sh
+++ dot_config/linak-controller/executable_literal_symlink_everything_here.sh
@@ -3,7 +3,6 @@ cd ~/Library/Application\ Support/linak-controller
 for i in $HOME/.config/linak-controller/*; do
     if [ "$i" != "$HOME/.config/linak-controller/symlink_everything_here.sh" ]; then
         echo "$i"
-        rm -rf $i
         ln -s "$i" .
     fi
 done
