8b1dc8a2bd4a045f67387f46000beef888f82d6e
Rafael Ribeiro <rafael.ntw@gmail.com>
2023-11-03 10:33:27 +0000

    Add .config/linak-controller/config.yaml Add .config/linak-controller/symlink_everything_here.sh

---
+++ dot_config/linak-controller/literal_symlink_everything_here.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd ~/Library/Application\ Support/linak-controller
+for i in /$HOME/.config/linak-controller/*; do
+    if [ "$i" != "/$HOME/.config/linak-controller/symlink_everything_here.sh" ]; then
+        echo "$i"
+        ln -s "$i" .
+    fi
+done
