9dd904aaee710d36f241ad3a8a4fa9a95ba3f9ac
rafaribe <rafael.ntw@gmail.com>
2024-06-23 17:52:45 +0100

    feat: load minio

--- dot_config/fish/functions/custom/minio.fish
+++ dot_config/fish/functions/custom/minio.fish
@@ -1,7 +1,6 @@
-#
-echo "Load Minio Tokens"
-
-set -gx AWS_ENDPOINT_URL_S3="https://s3.rafaribe.com"
-set -l MINIO_UUID "4bac1bcd-ef9e-4a60-97b2-af3201766c37"
-set -gx AWS_ACCESS_KEY_ID "$(bw get item $MINIO_UUID | jq -r '.fields[] | select(.name == "AWS_ACCESS_KEY_ID").value')"
-set -gx AWS_SECRET_ACCESS_KEY "$(bw get item $MINIO_UUID | jq -r '.fields[] | select(.name == "AWS_SECRET_ACCESS_KEY").value')"
+function load_minio
+    set -l MINIO_UUID "4bac1bcd-ef9e-4a60-97b2-af3201766c37"
+    set -gx AWS_ACCESS_KEY_ID "$(bw get item $MINIO_UUID | jq -r '.fields[] | select(.name == "AWS_ACCESS_KEY_ID").value')"
+    set -gx AWS_SECRET_ACCESS_KEY "$(bw get item $MINIO_UUID | jq -r '.fields[] | select(.name == "AWS_SECRET_ACCESS_KEY").value')"
+    set -gx AWS_ENDPOINT_URL_S3 "$(bw get item $MINIO_UUID | jq -r '.fields[] | select(.name == "AWS_ENDPOINT_URL_S3").value')"
+end
