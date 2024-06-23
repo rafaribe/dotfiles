50936824d149664672b43a755d1e34c5e4dd6f7e
rafaribe <rafael.ntw@gmail.com>
2024-06-23 17:47:20 +0100

    feat: add minio keys

---
+++ dot_config/fish/functions/custom/minio.fish
@@ -0,0 +1,7 @@
+#
+echo "Load Minio Tokens"
+
+set -gx AWS_ENDPOINT_URL_S3="https://s3.rafaribe.com"
+set -l MINIO_UUID "4bac1bcd-ef9e-4a60-97b2-af3201766c37"
+set -gx AWS_ACCESS_KEY_ID "$(bw get item $MINIO_UUID | jq -r '.fields[] | select(.name == "AWS_ACCESS_KEY_ID").value')"
+set -gx AWS_SECRET_ACCESS_KEY "$(bw get item $MINIO_UUID | jq -r '.fields[] | select(.name == "AWS_SECRET_ACCESS_KEY").value')"
